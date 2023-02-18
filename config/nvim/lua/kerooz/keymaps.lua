a6ddc61485f899d1ec84d2c2fa168260c71e8122
Yohann Genre <5088279-Yokann@users.noreply.github.com>
2023-02-19 00:23:41 +0100

    refactor install script

--- config/nvim/lua/kerooz/keymaps.lua
+++ config/nvim/lua/kerooz/keymaps.lua
@@ -10,8 +10,8 @@ vim.keymap.set("i", "<A-b>", "{}<Left>")
 -- Move block selection up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move block code up" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move block code down" })
-vim.keymap.set("n", "J", "<esc>:m .+1<CR>==" )
-vim.keymap.set("n", "K", "<esc>:m .-2<CR>==" )
+vim.keymap.set("n", "J", "<esc>:m .+1<CR>==", { noremap = false })
+vim.keymap.set("n", "K", "<esc>:m .-2<CR>==", { noremap = false })
 
 -- vim.keymap.set("n", "J", "mzJ`z") -- keep the cursor at the start of the line
 
@@ -22,7 +22,7 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
--- yank and keep in buffer after paste 
+-- yank and keep in buffer after paste
 vim.keymap.set("x", "<leader>p", "\"_dp")
 
 vim.keymap.set("n", "Q", "<nop>") -- don't binding anything on this key
