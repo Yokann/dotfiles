cef851a2a48ea83a04a4a13296053bc4a8c60108
Yohann Genre <ygenre.externe@bedrockstreaming.com>
2023-02-20 20:58:21 +0100

    find_files everywhere

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -12,6 +12,17 @@ telescope.setup({
             case_mode = "smart_case", -- or "ignore_case" or "respect_case"
             -- the default case_mode is "smart_case"
         }
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+            no_ignore = true,
+        },
+        live_grep = {
+            additional_args = function(opts)
+                return {"--hidden"}
+            end
+        }
     }
 })
 telescope.load_extension('fzf')
@@ -25,4 +36,5 @@ vim.keymap.set("n", '<leader>fh', builtin.help_tags, { desc = "[F]ind [H]elp" })
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymap" })
 vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "[L]ive [G]rep" })
 vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+vim.keymap.set("n", "<leader>fs", builtin.treesitter, { desc = "[F]ind [S]ymbols" })
 vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "[G]oto [R]eferences" })
