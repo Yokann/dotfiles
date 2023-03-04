52a812a0203aee59b280a0c590f7735337eb5732
Yohann Genre <5088279-Yokann@users.noreply.github.com>
2023-03-04 15:00:35 +0100

    nvim close nvim-tree before saving session

--- config/nvim/after/plugin/autosession.lua
+++ config/nvim/after/plugin/autosession.lua
@@ -6,5 +6,10 @@ end
 autosession.setup {
     log_level = "error",
     auto_session_suppress_dirs = { "~/" },
+    cwd_change_handling = {
+        restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
+        pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
+    },
+    pre_save_cmds = {require("nvim-tree.api").tree.close}
 }
 
