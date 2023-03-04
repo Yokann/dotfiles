49ebcc8c814f59a3aeb4635637590b5ee9426d0a
Yohann Genre <5088279-Yokann@users.noreply.github.com>
2023-03-04 01:40:41 +0100

    autocommand q on some popup

--- config/nvim/lua/kerooz/autocommand.lua
+++ config/nvim/lua/kerooz/autocommand.lua
@@ -4,3 +4,23 @@ vim.cmd([[
     autocmd BufWritePost .tmux.conf execute ':!tmux source-file %'
   augroup end
 ]])
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+  pattern = {
+    "qf",
+    "help",
+    "man",
+    "notify",
+    "lspinfo",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "PlenaryTestPopup",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
