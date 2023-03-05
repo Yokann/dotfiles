69c7476a506a7967f1042fda442b215a2616a020
Yohann Genre <5088279-Yokann@users.noreply.github.com>
2023-03-06 00:16:26 +0100

    nvim set diagnostic icons

--- config/nvim/after/plugin/lualine.lua
+++ config/nvim/after/plugin/lualine.lua
@@ -2,11 +2,35 @@ local ok, lualine = pcall(require, "lualine")
 if not ok then
     return
 end
-lualine.setup{}
--- lualine.setup {
---     options = {
---         disabled_filetypes = {
---             'NvimTree', 'DapBreakpoint', 'DapScope', 'DapStack'
---         }
---     }
--- }
+lualine.setup {
+    --     options = {
+    --         disabled_filetypes = {
+    --             'NvimTree', 'DapBreakpoint', 'DapScope', 'DapStack'
+    --         }
+    --     }
+    -- }
+    sections = {
+        lualine_b = {
+            { "branch", icon = "" },
+            {
+                "diff",
+                colored = true,
+                -- diff_color = {
+                --     added    = { fg = "#28A745" },
+                --     modified = { fg = "#DBAB09" },
+                --     removed  = { fg = "#D73A49" }
+                -- },
+                symbols = {
+                    added    = " ",
+                    modified = " ",
+                    removed  = " "
+                }
+            },
+            -- { "diagnostics' }
+        },
+        lualine_c = {
+            "diagnostics",
+            "filename",
+        }
+    }
+}
