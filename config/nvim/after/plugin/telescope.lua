077bac9a2c9b714336c4c32ba94116d3fb5755f1
Yohann Genre <ygenre.externe@bedrockstreaming.com>
2023-03-20 14:31:32 +0100

    nvim add telescope history

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -2,10 +2,22 @@ local ok, telescope = pcall(require, "telescope")
 if not ok then
     return
 end
+local actions = require('telescope.actions')
+
 
 telescope.setup({
     defaults = {
-        file_ignore_patterns = { "^%.git/" }
+        file_ignore_patterns = { "^%.git/" },
+        history = {
+            path = vim.fn.stdpath('data') .. '/telescope_history.sqlite3',
+            limit = 100,
+        },
+        mappings = {
+            i = {
+                ["<C-j>"] = actions.cycle_history_next,
+                ["<C-k>"] = actions.cycle_history_prev,
+            }
+        },
     },
     extensions = {
         fzf = {
@@ -44,11 +56,12 @@ telescope.setup({
 })
 telescope.load_extension('fzf')
 telescope.load_extension('ui-select')
+telescope.load_extension('smart_history')
 local builtin = require('telescope.builtin')
 
 -- Find 
-vim.keymap.set("n", '<leader><space>', builtin.find_files, { desc = "[F]ind in all [F]iles" })
-vim.keymap.set("n", '<leader>fg', builtin.git_files, { desc = "[F]ind in [G]it files" })
+vim.keymap.set("n", '<leader>ff', builtin.find_files, { desc = "[F]ind in all [F]iles" })
+vim.keymap.set("n", '<leader><space>', builtin.git_files, { desc = "[F]ind in [G]it files" })
 vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "[F]ind [r]ecently opened files" })
 vim.keymap.set("n", '<leader>fb', builtin.buffers, { desc = "[F]ind [B]uffers" })
 vim.keymap.set("n", '<leader>fh', builtin.help_tags, { desc = "[F]ind [H]elp" })
@@ -56,6 +69,6 @@ vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymap" })
 vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
 vim.keymap.set("n", "<leader>fs", builtin.treesitter, { desc = "[F]ind [S]ymbols" })
 -- Search
-vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "[L]ive [G]rep" })
+vim.keymap.set("n", "<leader>gg", builtin.live_grep, { desc = "Live [G]rep" })
 -- LSP
 vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "[G]oto [R]eferences" })
