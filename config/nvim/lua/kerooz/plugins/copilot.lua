457884421a60c1129a5c718a3ad28235a85ede6e
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2023-11-30 11:29:28 +0100

    nvim: add chatgpt

--- config/nvim/lua/kerooz/plugins/copilot.lua
+++ config/nvim/lua/kerooz/plugins/copilot.lua
@@ -41,4 +41,24 @@ return {
             debug = false,
         }
     },
+    {
+        "jackMort/ChatGPT.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("chatgpt").setup({
+                -- api_key_cmd = "op read op://Personal/OpenAI/api_credential --no-newline",
+                actions_paths = { "~/.config/nvim/lua/kerooz/config/chatgpt-actions.json" },
+            })
+        end,
+        keys = {
+            { "<leader>gpt",  "<cmd>ChatGPT<CR>",                    mode = "n",          desc = "ChatGPT - Launch" },
+            { "<leader>gpte", "<cmd>ChatGPTEditWithInstruction<CR>", mode = { "n", "v" }, desc = "ChatGPT - Edit with instruction" },
+            { "<leader>gptx", "<cmd>ChatGPTRun explain_code<CR>",    mode = { "n", "v" }, desc = "ChatGPT - Explain code" },
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
+    }
 }
