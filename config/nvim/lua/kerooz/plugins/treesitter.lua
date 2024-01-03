a734284e9e72fbf5ebf3d00e9c0ac32b5cf8f831
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2024-01-03 10:49:33 +0100

    nvim: fix treesitter BC

--- config/nvim/lua/kerooz/plugins/treesitter.lua
+++ config/nvim/lua/kerooz/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        lazy = false,
         build = function()
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
@@ -8,44 +9,47 @@ return {
         dependencies = {
             "JoosepAlviste/nvim-ts-context-commentstring",
         },
-        opts = {
-            sync_install = false,
-            ensure_installed = {
-                "bash",
-                "c",
-                "css",
-                "help",
-                "html",
-                "javascript",
-                "jq",
-                "jsonnet",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "tsx",
-                "typescript",
-                "vim",
-                "yaml",
-                "rust",
-                "php",
-                "go",
-                "gomod",
-                "gosum",
-                "terraform"
-            },
+        config = function()
+            local configs = require("nvim-treesitter.configs")
 
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            context_commentstring = {
-                enable = true,
-            }
-        }
+            configs.setup({
+                sync_install = false,
+                ensure_installed = {
+                    "bash",
+                    "c",
+                    "css",
+                    "vimdoc",
+                    "html",
+                    "javascript",
+                    "jq",
+                    "jsonnet",
+                    "json",
+                    "lua",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "query",
+                    "regex",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "yaml",
+                    "rust",
+                    "php",
+                    "go",
+                    "gomod",
+                    "gosum",
+                    "terraform"
+                },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                }
+            })
+            require("ts_context_commentstring").setup({
+                enable_autocmd = true,
+            })
+        end
     },
     {
         "nvim-treesitter/playground",
