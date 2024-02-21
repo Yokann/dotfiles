1eac084e220b5e3daccb221312d763b7031b6601
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2024-02-21 09:41:22 +0100

    hypr: add setup

---
+++ setup/hyperland.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# https://github.com/natpen/awesome-wayland
+
+# Install hyprland and core dependencies
+yay -S hyprland wdisplays foot foot-terminfo waybar sworkstyle mako-git wev xdg-desktop-portal-wlr
+# xdg-desktop-portal-hyprland-git
+
+# Install additional dependencies
+yay -S wl-clipboard cliphist \ # clipboard
+    pipewire wireplumber \ # audio
+    grimshot swappy \ # screenshot
+    wf-recorder \ # screen recording
+    swaylock-effects-git \ # lockscreen
+    swww \ # wallpaper 
+    swayidle-git \ # idle
+    brightnessctl # brightness
+
+# Themes
+yay -S socat geticons
