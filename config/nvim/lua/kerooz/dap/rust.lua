e5fef1ca95fa500fd87192242369d4a01cffe91c
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2024-05-14 10:24:52 +0200

    nvim: add rust dap config

--- config/nvim/lua/kerooz/dap/rust.lua
+++ config/nvim/lua/kerooz/dap/rust.lua
@@ -2,14 +2,14 @@ local M = {}
 
 function M.setup()
     local dap = require "dap"
-    -- dap.adapters.codelldb = {
-    --     type = 'server',
-    --     port = "${port}",
-    --     executable = {
-    --         command = '/usr/bin/codelldb',
-    --         args = { "--port", "${port}" },
-    --     }
-    -- }
+    dap.adapters.codelldb = {
+        type = 'server',
+        port = "${port}",
+        executable = {
+            command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+            args = { "--port", "${port}" },
+        }
+    }
     dap.adapters.lldb = {
         type = "executable",
         command = "/usr/bin/lldb-vscode", -- adjust as needed
@@ -18,18 +18,19 @@ function M.setup()
     dap.configurations.rust = {
         {
             name = "Rust debug",
-            type = "lldb",
+            type = "codelldb",
             request = "launch",
             program = function()
                 return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
             end,
             options = {
-                env = {
-                    TWT_TOKEN = "oauth:t2pnngdx9lbqxuwcqd5si8167g77d2"
-                }
+                showDisassembly = "never",
+            --     env = {
+            --         TWT_TOKEN = "oauth:t2pnngdx9lbqxuwcqd5si8167g77d2"
+            --     }
             },
             cwd = '${workspaceFolder}',
-            stopOnEntry = true,
+            stopOnEntry = false,
         },
     }
 end
