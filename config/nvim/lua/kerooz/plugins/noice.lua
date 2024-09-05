cdf68f043947914651e22d12c2897c50b1919e0f
Yohann <5088279+Yokann@users.noreply.github.com>
2024-09-06 01:48:02 +0200

    nvim: disable notify

--- config/nvim/lua/kerooz/plugins/noice.lua
+++ config/nvim/lua/kerooz/plugins/noice.lua
@@ -9,9 +9,9 @@ return {
             -- NOTE: If you enable messages, then the cmdline is enabled automatically.
             -- This is a current Neovim limitation.
             enabled = true,              -- enables the Noice messages UI
-            view = "notify",             -- default view for messages
-            view_error = "notify",       -- view for errors
-            view_warn = "notify",        -- view for warnings
+            view = "mini",               -- default view for messages
+            view_error = "mini",         -- view for errors
+            view_warn = "mini",          -- view for warnings
             view_history = "messages",   -- view for :messages
             view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
         },
@@ -69,7 +69,7 @@ return {
             "rcarriga/nvim-notify",
             opts = {
                 stages = "fade",
-                timeout = 2000,
+                timeout = 1000,
             }
         },
         "MunifTanjim/nui.nvim",
