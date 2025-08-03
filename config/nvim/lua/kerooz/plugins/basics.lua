6a43b37f7e9f64ce8c4b429a092e55fa821da9be
Yohann <5088279+Yokann@users.noreply.github.com>
2025-08-03 14:59:58 +0200

    nvim: fix autopair dependency

--- config/nvim/lua/kerooz/plugins/basics.lua
+++ config/nvim/lua/kerooz/plugins/basics.lua
@@ -48,7 +48,10 @@ return {
             local cmp_autopairs = require("nvim-autopairs.completion.cmp")
             local cmp = require("cmp")
             cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end
+        end,
+        dependencies = {
+            "hrsh7th/nvim-cmp",
+        }
     },
     {
         "rmagatti/auto-session", -- restore previous dir session
