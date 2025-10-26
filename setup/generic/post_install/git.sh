6bfdd9783ea289b79d90d1ee9134b23df5f1dd3c
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2025-10-26 11:04:14 +0100

    git: add theme config delta

--- setup/generic/post_install/git.sh
+++ setup/generic/post_install/git.sh
@@ -1,2 +1,3 @@
 git config --global include.path $DOTFILES_PATH/config/git/gitconfig
 git config --global core.excludesFile $DOTFILES_PATH/config/git/global-gitignore
+git config --global delta.features catpuccin-macchiato
