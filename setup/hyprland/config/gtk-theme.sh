3cae3c8a3422195770ff0dbac1a653d29fa105cb
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2025-10-26 18:31:59 +0100

    setup: avoid install gtk-theme everytime

--- setup/hyprland/config/gtk-theme.sh
+++ setup/hyprland/config/gtk-theme.sh
@@ -1,3 +1,5 @@
-git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git /tmp/gtk-catppuccin
-/tmp/gtk-catppuccin/themes/install.sh --tweaks macchiato -t lavender
-rm -rf /tmp/gtk-catppuccin
+if [ ! -d "$HOME/.themes/Catppuccin-Purple-Dark-Macchiato" ]; then
+    git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git /tmp/gtk-catppuccin
+    /tmp/gtk-catppuccin/themes/install.sh --tweaks macchiato -t lavender
+    rm -rf /tmp/gtk-catppuccin
+fi
