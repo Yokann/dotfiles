3b387d1840a83a591eb749849aa05d2e43700c30
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2025-10-26 16:37:58 +0100

    hypr: complete basic monitor

--- setup/hyprland/config/hyprland.sh
+++ setup/hyprland/config/hyprland.sh
@@ -14,6 +14,8 @@ if [ $STATE_FLAG = "first-install" ]; then
     echo '$configPath = $DOTFILES_PATH/config/hypr' > $XDG_HYPR_CONFIG_PATH/hyprland.conf
     echo '$wallpapersPath = $HOME/Images/wallpapers' >> $XDG_HYPR_CONFIG_PATH/hyprland.conf
     echo '$machineType = laptop' >> $XDG_HYPR_CONFIG_PATH/hyprland.conf
+    echo "# To get monitors list: hyprctl monitors | grep 'Monitor' | awk '{print \$2}'" >> $XDG_HYPR_CONFIG_PATH/hyprland.conf
     echo '# env = HYPR_MAIN_MONITOR, DP-1' >> $XDG_HYPR_CONFIG_PATH/hyprland.conf
+    echo '# monitor = DP-1, 1920x1080@60, 0x0, 1' >> $XDG_HYPR_CONFIG_PATH/hyprland.conf
     echo 'source = $configPath/hyprland.conf' >> $XDG_HYPR_CONFIG_PATH/hyprland.conf
 fi
