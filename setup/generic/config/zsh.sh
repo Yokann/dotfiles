8fb2aa51b642a2088bd9d3126c4b3ebdb3805578
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2025-11-01 15:52:54 +0100

    fixup! setup: edit zshrc only if it doesn't contain base sourcing

--- setup/generic/config/zsh.sh
+++ setup/generic/config/zsh.sh
@@ -1,10 +1,10 @@
-if [ cat .zshrc | grep "source $DOTFILES_PATH/config/zsh/.zshrc" >/dev/null 2>&1 ]; then
-    echo "Zsh is already configured. Skipping..."
-else
+if ! grep -q "source $DOTFILES_PATH/config/zsh/.zshrc" "$HOME/.zshrc" 2>/dev/null; then
     CURRENT_DATE=$(date "+%Y-%m-%d-%H-%M-%S")
-    mkdir -p $HOME/.cache/zinit/completions
+    mkdir -p "$HOME/.cache/zinit/completions"
     if [ -f $HOME/.zshrc ]; then
         cp $HOME/.zshrc "$HOME/.zshrc.bak$CURRENT_DATE"
     fi
-    echo "source $DOTFILES_PATH/config/zsh/.zshrc" > $HOME/.zshrc
+    echo "source $DOTFILES_PATH/config/zsh/.zshrc" > "$HOME/.zshrc"
+else
+    echo "Zsh is already configured. Skipping..."
 fi
