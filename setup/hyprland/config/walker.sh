a5930c654764b71e908c397afc7f6ecdb57d7822
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2025-11-26 13:11:33 +0100

    setup: run elephant via systemd

--- setup/hyprland/config/walker.sh
+++ setup/hyprland/config/walker.sh
@@ -2,3 +2,4 @@ rm -f $HOME/.config/walker
 ln -s $DOTFILES_PATH/config/hypr/themes/2024/dots/walker $HOME/.config/walker
 rm -f $HOME/.config/elephant
 ln -s $DOTFILES_PATH/config/hypr/themes/2024/dots/elephant $HOME/.config/elephant
+elephant service enable
