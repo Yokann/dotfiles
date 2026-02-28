0979196462bcd26c8b956f1008cb90407b8eb129
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2026-02-28 16:30:11 +0100

    nvim: fix gotmpl with helm

--- config/nvim/lua/config/filetype.lua
+++ config/nvim/lua/config/filetype.lua
@@ -1,6 +1,7 @@
 vim.filetype.add({
     pattern  = {
         [".*/.cloud/helm/.*.gotmpl"] = "helm",
+        [".*/.cloud/charts/.*.gotmpl"] = "helm",
         [".*/k8s/app/.*.gotmpl"] = "helm",
-   },
+    },
 })
