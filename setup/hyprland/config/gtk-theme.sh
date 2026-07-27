87cc6172d2c57fe293a52736adb0e01481acbd79
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2026-07-27 11:51:33 +0200

    chore(hypr): update Catppuccin-GTK theme

--- setup/hyprland/config/gtk-theme.sh
+++ setup/hyprland/config/gtk-theme.sh
@@ -1,11 +1,11 @@
-if [ ! -d "$HOME/.themes/Catppuccin-Lavender-Dark-Macchiato" ]; then
-    git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git /tmp/gtk-catppuccin
-    /tmp/gtk-catppuccin/themes/install.sh --tweaks macchiato -t lavender
+if [ ! -d "$HOME/.themes/Catppuccin-Macchiato-Lavender-Dark" ]; then
     rm -rf /tmp/gtk-catppuccin
+    git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git /tmp/gtk-catppuccin
+    /tmp/gtk-catppuccin/themes/install.sh --tweaks macchiato -a lavender -m dark --libadwaita
 fi
 
 mkdir -p $HOME/.config/qt6ct/colors
 if [ ! -f "$HOME/.config/qt6ct/colors/catppuccin-macchiato-lavender.conf" ]; then
     wget https://github.com/catppuccin/qt5ct/raw/refs/heads/main/themes/catppuccin-macchiato-lavender.conf \
-    -O $HOME/.config/qt6ct/colors/catppuccin-macchiato-lavender.conf
+        -O $HOME/.config/qt6ct/colors/catppuccin-macchiato-lavender.conf
 fi
