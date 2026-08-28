97965b30c6aa986dc476e1e74e6fc24e1c3940fe
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2026-08-28 13:36:34 +0200

    chore(hypr): refactor theme dots (walker)

--- setup/hyprland/config/walker.sh
+++ setup/hyprland/config/walker.sh
@@ -1,5 +1,5 @@
 rm -f $HOME/.config/walker
-ln -s $DOTFILES_PATH/config/hypr/themes/2024/dots/walker $HOME/.config/walker
+ln -s $DOTFILES_PATH/config/walker $HOME/.config/walker
 rm -f $HOME/.config/elephant
 ln -s $DOTFILES_PATH/config/elephant $HOME/.config/elephant
 if [[ $(systemctl --user is-enabled elephant) != "enabled" ]]; then
