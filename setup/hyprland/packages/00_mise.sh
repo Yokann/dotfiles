da45c5c1099d74038e01d778cb7117d18161a547
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2026-08-28 15:47:19 +0200

    fixup! hypr: use mise to provide go and nodejs

--- setup/hyprland/packages/00_mise.sh
+++ setup/hyprland/packages/00_mise.sh
@@ -3,7 +3,7 @@ if [ -f "$HOME/.local/bin/mise" ]; then
     echo "Mise installed successfully!"
     eval "$(mise activate --shims)"
     mise use -g go
-    mise use -g node npm
+    mise use -g node npm yarn pnpm
     # Setup dummy package to replace the default package manager with Mise
     for pkg in "$DOTFILES_PATH/setup/hypr/assets/pkg/*"; do
         makepkg -si --noconfirm -D $pkg
