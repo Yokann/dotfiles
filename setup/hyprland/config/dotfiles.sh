2704b1aa6899f0a1fd19b12e9014efccae76b293
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2026-08-29 16:12:07 +0200

    fixup! chore(hypr): define env inside ~/.config/profile.d

--- setup/hyprland/config/dotfiles.sh
+++ setup/hyprland/config/dotfiles.sh
@@ -1,15 +1,13 @@
-if [ ! -f "/etc/profile.d/dotfiles.sh" ]; then
-    cat >/tmp/dotfiles.sh <<EOF
+if [ ! -f "$HOME/.config/profile.d/00-dotfiles.sh" ]; then
+    cat >$HOME/.config/profile.d/00-dotfiles.sh <<EOF
 export DOTFILES_PATH="$DOTFILES_PATH"
 EOF
-    sudo cp /tmp/dotfiles.sh /etc/profile.d/dotfiles.sh
 fi
 
 if [ ! -f "$HOME/.config/environment.d/00-dotfiles.conf" ]; then
     mkdir -p $HOME/.config/environment.d
     cat >$HOME/.config/environment.d/10-dotfiles.conf <<EOF
 DOTFILES_PATH=$DOTFILES_PATH
-PATH=${HOME}/.local/bin:${HOME}/.cargo/bin:${HOME}/go/bin:${PATH}
 EOF
 fi
 
