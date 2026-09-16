554e0119e1c2b9419bd9f40b225cc9e2f3e30de3
Yohann Genre <5088279+Yokann@users.noreply.github.com>
2026-09-16 16:03:53 +0200

    chore(setup): install mise from signed repository

--- setup/hyprland/packages/00_mise.sh
+++ setup/hyprland/packages/00_mise.sh
@@ -1,12 +1,13 @@
-curl https://mise.run | sh
-if [ -f "$HOME/.local/bin/mise" ]; then
+sudo pacman -S --noconfirm --needed mise
+
+if command -v mise &>/dev/null; then
     echo "Mise installed successfully!"
     eval "$(mise activate --shims)"
     mise use -g go
     mise use -g node npm yarn pnpm
     # Setup dummy package to replace the default package manager with Mise
-    for pkg in "$DOTFILES_PATH/setup/hypr/assets/pkg/*"; do
-        makepkg -si --noconfirm -D $pkg
+    for pkg in "$DOTFILES_PATH"/setup/hyprland/assets/pkg/*; do
+        makepkg -si --noconfirm -D "$pkg"
     done
 else
     echo "Failed to install Mise. Please check the installation script for errors."
